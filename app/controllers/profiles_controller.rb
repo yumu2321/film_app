7b37653982bf2c66f917b95e795b65209e221c15
yumu2321 <yumu2321@gmail.com>
2022-05-18 11:49:20 +0000

    mypage5

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,23 @@
+class ProfilesController < ApplicationController
+
+    def edit
+      @user = User.find(current_user.id)
+    end
+    
+    def update
+      @user = User.find(current_user.id)
+      if @user.update(user_params)
+        redirect_to "/users/#{current_user.id}", success: "ユーザーを更新しました"
+      else
+        flash.now['danger'] = "ユーザーを更新できませんでした"
+        render :edit 
+      end
+    end
+    
+    private
+    
+    def user_params
+      params.require(:user).permit(:email, :nickname, :avatar)
+    end
+    
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+    
     def show
         @user = User.find(params[:id])
         @email = current_user.email
         @posts = current_user.posts
     end
+    
 end
